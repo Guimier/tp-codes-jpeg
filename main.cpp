d692bd66f900128e14ccc5b8cbe677f646e1a8d2
hoangjimmy <yumiquet@gmail.com>
2015-10-22 10:28:37 +0200

    Implementation du decodage

--- main.cpp
+++ main.cpp
@@ -49,6 +49,44 @@ void ComputeFullPrecisionDctBloc( CImg<unsigned char> bloc, CImg<signed char>& s
 	}
 }
 
+void MultiplyMatrix( const CImg<double> matrix,const CImg<unsigned char> cropedCompress , CImg<unsigned char>  cropedOrigin){
+	
+	for(int i =0 ; i< BLOC_SIZE ;++i){
+		for(int j=0; j<= BLOC_SIZE;++j){
+			cropedOrigin(i,j) = cropedCompress(i,j) * matrix(i,j);
+		}
+	}	
+}
+
+double ComputeInvertDctPoint(const CImg<unsigned char> bloc, int i, int j){
+	#define C(i) ((i)?1:(1/sqrt(2)))
+	double sum = 0;
+	
+		for ( int x = 0; x < BLOC_SIZE; ++x ) {
+			for ( int y = 0; y < BLOC_SIZE; ++y ) {
+				sum += bloc( x, y )
+				*C(i)
+				*C(j)
+				* cos( ( 2 * x + 1 ) * i * cimg::PI / 2. / BLOC_SIZE )
+				* cos( ( 2 * y + 1 ) * j * cimg::PI / 2. / BLOC_SIZE );
+		}
+	}
+
+	return sum * 2 / BLOC_SIZE;
+}
+void ComputeInvertDctBloc( CImg<unsigned char> bloc,CImg<double>& cbloc ){
+		for ( int i = 0; i < BLOC_SIZE; ++i ) {
+			for ( int j = 0; j < BLOC_SIZE; ++j ){
+				cbloc ( i,j) = ComputeInvertDctPoint(bloc,i,j);
+			}
+		}
+		for ( int x = 0; x < BLOC_SIZE; ++x ) {
+			for ( int y = 0; y < BLOC_SIZE; ++y ) {
+				cbloc( x, y ) = cbloc( x, y ) + 128;
+		}
+	}
+}
+
 CImg<unsigned char> JPEGEncoder( const CImg<unsigned char> image, const float q ) {
 	CImg<double> fullPrecision( BLOC_SIZE, BLOC_SIZE, 1, 1, 0 );
 	CImg<signed char> shifted( BLOC_SIZE, BLOC_SIZE, 1, 1, 0 );
@@ -80,6 +118,29 @@ CImg<unsigned char> JPEGEncoder( const CImg<unsigned char> image, const float q 
 	return comp;
 }
 
+CImg <unsigned char> JPEGDecoder (const CImg<unsigned char> compressedImage, const float q){
+	CImg<double> fullPrecision( BLOC_SIZE, BLOC_SIZE, 1, 1, 0 );
+	CImg <unsigned char> decompressImage(compressedImage.width(),compressedImage.height(),1,1,0);
+	CImg<double> Q( 8, 8 );
+	Q(0,0)=q*16;   Q(0,1)=q*11;   Q(0,2)=q*10;   Q(0,3)=q*16;   Q(0,4)=q*24;   Q(0,5)=q*40;   Q(0,6)=q*51;   Q(0,7)=q*61;
+	Q(1,0)=q*12;   Q(1,1)=q*12;   Q(1,2)=q*14;   Q(1,3)=q*19;   Q(1,4)=q*26;   Q(1,5)=q*58;   Q(1,6)=q*60;   Q(1,7)=q*55;
+	Q(2,0)=q*14;   Q(2,1)=q*13;   Q(2,2)=q*16;   Q(2,3)=q*24;   Q(2,4)=q*40;   Q(2,5)=q*57;   Q(2,6)=q*69;   Q(2,7)=q*56;
+	Q(3,0)=q*14;   Q(3,1)=q*17;   Q(3,2)=q*22;   Q(3,3)=q*29;   Q(3,4)=q*51;   Q(3,5)=q*87;   Q(3,6)=q*80;   Q(3,7)=q*62;
+	Q(4,0)=q*18;   Q(4,1)=q*22;   Q(4,2)=q*37;   Q(4,3)=q*56;   Q(4,4)=q*68;   Q(4,5)=q*109;  Q(4,6)=q*103;  Q(4,7)=q*77;
+	Q(5,0)=q*24;   Q(5,1)=q*35;   Q(5,2)=q*55;   Q(5,3)=q*64;   Q(5,4)=q*81;   Q(5,5)=q*104;  Q(5,6)=q*113;  Q(5,7)=q*92;
+	Q(6,0)=q*49;   Q(6,1)=q*64;   Q(6,2)=q*78;   Q(6,3)=q*87;   Q(6,4)=q*103;  Q(6,5)=q*121;  Q(6,6)=q*120;  Q(6,7)=q*101;
+	Q(7,0)=q*72;   Q(7,1)=q*92;   Q(7,2)=q*95;   Q(7,3)=q*98;   Q(7,4)=q*112;  Q(7,5)=q*100;  Q(7,6)=q*103;  Q(7,7)=q*99;
+
+	for ( int i = 0; i < compressedImage.width(); i += BLOC_SIZE ) {
+		for ( int j = 0; j < compressedImage.height(); j += BLOC_SIZE ) {
+			MultiplyMatrix(Q,compressedImage.get_crop(i, j, i + BLOC_SIZE, j + BLOC_SIZE ), decompressImage.get_crop(i, j, i + BLOC_SIZE, j + BLOC_SIZE ));
+			ComputeInvertDctBloc(decompressImage.get_crop(i, j, i + BLOC_SIZE, j + BLOC_SIZE ),fullPrecision);
+		}
+	
+	}
+	return decompressImage;
+}
+
 int main()
 {
 	// Read the image "lena.bmp"
@@ -88,14 +149,16 @@ int main()
 	// Take the luminance information 
 	my_image.channel(0);
 
-	float quality=1.;
+	float quality=0.5;
 	CImg<unsigned char> comp_image = JPEGEncoder( my_image, quality );
+	CImg<unsigned char> decomp_image = JPEGDecoder(comp_image,quality);
 
 	// Display the bmp file
 	CImgDisplay main_disp( my_image, "Initial Image" );
 
 	// Display the compressed file (by dct)
 	CImgDisplay comp_disp( comp_image, "Compressed Image" );
+	CImgDisplay decomp_disp( decomp_image, "Decompressed Image" );
 
 	while ( ! main_disp.is_closed() ) {
 		main_disp.wait();
